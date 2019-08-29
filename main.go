7c8ac651132e9b9108a1ddee4a0e049497668488
Sourav Sett <sourav241196@gmail.com>
2019-08-29 14:50:38 +0530

    1st commit

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"strings"
+)
+
+//Date should be written in mmm-dd-yyyy format with month being a 3 lettered abbreviation and 1st letter capital
+//Date shouldn't preced more than a week from present date
+func DateScrape(w http.ResponseWriter, r *http.Request) {
+	// Request the HTML page.
+	res, err := http.Get("http://eduro.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	vars := mux.Vars(r)
+	date := strings.Split(vars["date"], "-")
+	quote := ""
+	// Find quote of the day
+	doc.Find(".singlemeta").Each(func(i int, s *goquery.Selection) {
+		if date[0] == s.Find(".months").Text() && date[1] == s.Find(".dates").Text() && date[2] == s.Find(".years").Text() {
+			// get the quote
+			quote = s.Find("p").Text()
+			w.Write([]byte(quote))
+			return
+		}
+	})
+}
+
+func TodayScrape(w http.ResponseWriter, _ *http.Request) {
+	// Request the HTML page.
+	res, err := http.Get("http://eduro.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	quote := ""
+	//find quote of the day
+	doc.Find(".singlemeta").Each(func(i int, s *goquery.Selection) {
+		if i < 1 {
+			// get the quote
+			quote = s.Find("p").Text()
+			w.Write([]byte(quote))
+		}
+	})
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/{date}", DateScrape)
+	router.HandleFunc("/", TodayScrape)
+
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
